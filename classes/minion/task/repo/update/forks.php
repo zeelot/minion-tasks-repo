59e42ebe351776c1814af34ce6cbc72bef11801a
Lorenzo Pisani <zeelot3k@gmail.com>
2011-01-28 22:58:52 -0700

    initial implementation, depends on the Zeelot/kohana-git module

---
+++ classes/minion/task/repo/update/forks.php
@@ -0,0 +1,74 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Minion_Task_Repo_Update_Forks extends Minion_Task {
+
+	public function execute(array $config)
+	{
+		$modules = Kohana::config('minion-repo-forks');
+
+		foreach ($modules as $path => $options)
+		{
+			$repo = new Git($path);
+
+			CLI::output('# '.$path);
+
+			CLI::output('Adding remotes...');
+
+			CLI::output('git remote add minion-fetch '.$options['fetch_from']);
+			CLI::output('git remote add minion-push '.$options['push_to']);
+
+			// Make sure both minion remotes are created
+			try
+			{
+				$repo->execute('remote add minion-fetch '.$options['fetch_from']);
+			}
+			catch (Exception $e) {} // Remote must already exist (ignore)
+
+			try
+			{
+				$repo->execute('remote add minion-push '.$options['push_to']);
+			}
+			catch (Exception $e) {} // Remote must already exist (ignore)
+
+			$repo->execute('fetch minion-fetch');
+
+			try
+			{
+				// We need to find the branches
+				$branches = array_map('trim', explode(PHP_EOL, trim($repo->execute('branch -a'))));
+				foreach ($branches as $branch)
+				{
+					if (substr($branch, 0, 21) === 'remotes/minion-fetch/')
+					{
+						$local = substr($branch, 21);
+						CLI::output('# Updating Branch "'.$local.'" ... ', FALSE);
+
+						$repo->execute('checkout -b '.$local.' '.$branch);
+						$repo->execute('push minion-push '.$local);
+						$repo->execute('checkout '.$branch);
+						$repo->execute('branch -D '.$local);
+
+						CLI::output('Done.');
+					}
+				}
+			}
+			catch (Exception $e)
+			{
+				CLI::output('');
+				CLI::output('######################');
+				CLI::output('# Problem Encountered:');
+				CLI::output($e->getMessage());
+				CLI::output('######################');
+			}
+
+			// Always clean up our remotes!
+			$repo->execute('remote rm minion-fetch');
+			$repo->execute('remote rm minion-push');
+
+			// Update 'origin' now
+			$repo->execute('fetch origin');
+			$repo->execute('checkout '.$options['checkout']);
+			CLI::output('Switched to branch "'.$options['checkout'].'"');
+		}
+	}
+}
---
+++ config/minion-repo-forks.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	/**
+	 * Set all the modules you would like to update using the following structure:
+	 * 
+	 *     MODPATH.'auth' => array(
+	 *         'fetch_from' => 'https://github.com/kohana/auth.git',
+	 *         'push_to'    => 'git@github.com:synapsestudios/kohana-auth.git',
+	 *         'checkout'   => 'origin/3.1/develop',
+	 *     ),
+	 */
+);
